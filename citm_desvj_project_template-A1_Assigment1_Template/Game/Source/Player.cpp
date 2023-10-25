906a8c30e5ce8f29058889f99c89367dc858284c
Sergio <sergiof.sf88@gmail.com>
2023-10-25 12:45:36 +0200

    upload game

---
+++ citm_desvj_project_template-A1_Assigment1_Template/Game/Source/Player.cpp
@@ -0,0 +1,174 @@
+#include "Player.h"
+#include "App.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Render.h"
+#include "Scene.h"
+#include "Log.h"
+#include "Point.h"
+#include "Physics.h"
+
+Player::Player() : Entity(EntityType::PLAYER)
+{
+	name.Create("Player");
+}
+
+Player::~Player() {
+
+}
+
+bool Player::Awake() {
+
+	position.x = parameters.attribute("x").as_int();
+	position.y = parameters.attribute("y").as_int();
+	texturePath = parameters.attribute("texturepath").as_string();
+
+	return true;
+}
+
+bool Player::Start() {
+
+	idleAnim.PushBack({ 98, 468, 47, 65 });
+	idleAnim.PushBack({ 165, 468, 47, 65 });
+	idleAnim.PushBack({ 232, 468, 47, 65 });
+	idleAnim.PushBack({ 299, 468, 47, 65 });
+	idleAnim.speed = 0.1f;
+	idleAnim.loop = true;
+
+	walkAnim.PushBack({94,44,52,66});
+	walkAnim.PushBack({160,44,52,66});
+	walkAnim.PushBack({231,44,52,66});
+	walkAnim.PushBack({305,44,52,66});
+	walkAnim.PushBack({377,44,52,66});
+	walkAnim.PushBack({429,44,52,66});
+	walkAnim.PushBack({499,44,52,66});
+	walkAnim.PushBack({567,44,52,66});
+	walkAnim.speed = 0.1f;
+	walkAnim.loop = true;
+
+	atack1Anim.PushBack({ 77,823,87,76 });
+	atack1Anim.PushBack({ 154,823,87,76 });
+	atack1Anim.PushBack({ 238,823,87,76 });
+	atack1Anim.PushBack({ 328,823,87,76 });
+	atack1Anim.PushBack({400,823,87,76});
+	atack1Anim.speed = 0.1f;
+	atack1Anim.loop = true;
+
+	deadAnim.PushBack({ 91,743,65,66 });
+	deadAnim.PushBack({ 164,743,65,66 });
+	deadAnim.PushBack({ 246,743,65,66 });
+	deadAnim.PushBack({ 325,743,65,66 });
+	deadAnim.PushBack({ 402,743,65,66 });
+	deadAnim.PushBack({ 478,743,65,66 });
+	deadAnim.speed = 0.1f;
+	deadAnim.loop = true;
+
+	//initilize textures
+	texture = app->tex->Load(texturePath);
+	currentAnimation = &idleAnim;
+
+	pbody = app->physics->CreateRectangle(position.x, position.y, 44,64, bodyType::DYNAMIC);
+	pbody->listener = this;
+	pbody->ctype = ColliderType::PLAYER;
+
+	//pickCoinFxId = app->audio->LoadFx("Assets/Audio/Fx/retro-video-game-coin-pickup-38299.ogg");
+
+	
+	return true;
+}
+
+bool Player::Update(float dt)
+{
+
+	flipPos.x = position.x - 10;
+	b2Vec2 currentVelocity = pbody->body->GetLinearVelocity();
+	//b2Vec2 vel = b2Vec2(0, -GRAVITY_Y);
+
+	currentAnimation = &idleAnim;
+	
+	if (app->input->GetKey(SDL_SCANCODE_A) == KEY_IDLE && app->input->GetKey(SDL_SCANCODE_D) == KEY_IDLE)
+	{
+		currentVelocity.x = 0;
+	}
+
+	if (app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) {
+		currentAnimation = &atack1Anim;
+	}
+	if (app->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) {
+		currentAnimation = &deadAnim;
+	}
+
+	if (app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+		right = false;
+		//vel = b2Vec2(-speed*dt, -GRAVITY_Y);
+		currentVelocity.x = -speed * dt;
+		currentAnimation = &walkAnim;
+	}
+
+	if (app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+		right = true;
+		currentVelocity.x = +speed * dt;
+		//vel = b2Vec2(speed*dt, -GRAVITY_Y);
+		currentAnimation = &walkAnim;
+	}
+	if (app->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN && !jump) {
+		jump = true;
+		currentVelocity.y = -0.5 * dt;
+		pbody->body->SetLinearVelocity(currentVelocity);
+	}
+
+	//Set the velocity of the pbody of the player
+
+	if (jump == false)
+	{
+		currentVelocity.y = +0.4 * dt;
+	}
+
+	//Set the velocity of the pbody of the player
+	pbody->body->SetLinearVelocity(currentVelocity);
+
+	//Update player position in pixels
+	position.x = METERS_TO_PIXELS(pbody->body->GetTransform().p.x) - 22;
+	position.y = METERS_TO_PIXELS(pbody->body->GetTransform().p.y) - 32;
+
+	SDL_Rect rect = currentAnimation->GetCurrentFrame();
+
+	if (right)
+	{
+		app->render->DrawTexture(texture, position.x, position.y, &rect);
+	}
+	else
+	{
+		
+		app->render->DrawTexture(texture, flipPos.x, position.y, &rect,1.0f,0, INT_MAX,INT_MAX,SDL_FLIP_HORIZONTAL);
+	}
+	
+	currentAnimation->Update();
+
+	return true;
+}
+
+bool Player::CleanUp()
+{
+
+	return true;
+}
+
+void Player::OnCollision(PhysBody* physA, PhysBody* physB) {
+
+	switch (physB->ctype)
+	{
+	case ColliderType::ITEM:
+		LOG("Collision ITEM");
+		app->audio->PlayFx(pickCoinFxId);
+		break;
+	case ColliderType::PLATFORM:
+		jump = false;
+		LOG("Collision PLATFORM");
+		break;
+	case ColliderType::UNKNOWN:
+		LOG("Collision UNKNOWN");
+		break;
+	}
+}
---
+++ citm_desvj_project_template-A1_Assigment1_Template/Game/Source/Player.h
@@ -0,0 +1,53 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include "Entity.h"
+#include "Point.h"
+#include "Animation.h"
+#include "SDL/include/SDL.h"
+
+struct SDL_Texture;
+
+class Player : public Entity
+{
+public:
+
+	Player();
+	
+	virtual ~Player();
+
+	bool Awake();
+
+	bool Start();
+
+	bool Update(float dt);
+
+	bool CleanUp();
+
+	void OnCollision(PhysBody* physA, PhysBody* physB);
+
+public:
+	float speed = 0.4f;
+	const char* texturePath;
+	SDL_Texture* texture = NULL;
+	PhysBody* pbody;
+	bool jump = false;
+	int pickCoinFxId;
+	bool right;
+
+	iPoint flipPos;
+
+	Animation* currentAnimation = nullptr;
+	Animation idleAnim;
+	Animation walkAnim;
+	Animation atack1Anim;
+	Animation atack2Anim;
+	Animation atack3Anim;
+	Animation jumpAnim;
+	Animation deadAnim;
+	Animation defendAnim;
+	Animation hurtAnim;
+	Animation runAnim;
+};
+
+#endif // __PLAYER_H__
