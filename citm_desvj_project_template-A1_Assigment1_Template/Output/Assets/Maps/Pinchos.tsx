7d3bc99d5af63f555c1a2d29973bb9855c393c5b
PauloWhite2004 <125252995+PauloWhite2004@users.noreply.github.com>
2023-10-30 14:15:12 +0100

    Mapa 2

---
+++ citm_desvj_project_template-A1_Assigment1_Template/Output/Assets/Maps/Pinchos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Pinchos" tilewidth="32" tileheight="32" tilecount="120" columns="10">
+ <image source="../../../../../../../Desktop/Tree-Assets.png" width="336" height="400"/>
+</tileset>
